4c6b16b907d790328f84a5ce4e06be90ff778cfa
Mohd Azrulnizam <azrulnizam.ghani@gmail.com>
2024-11-12 12:50:43 +0800

    bugfix Sass @import rules are deprecated

--- fe/vite.config.js
+++ fe/vite.config.js
@@ -1,16 +1,25 @@
 import { fileURLToPath, URL } from 'node:url';
-
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
-export default defineConfig(() => {
-    return {
-        plugins: [vue()],
-        resolve: {
-            alias: {
-                '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig({
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                // Add the includePaths option for Sass to resolve aliases
+                additionalData: `@use '@/assets/layout/layout' as *;`,
+                quietDeps: true,  // This line enables the --quiet-deps behavior
             }
         }
-    };
+    },
+    scss: {
+        api: 'modern-compiler',
+        quietDeps: true
+      },
 });
